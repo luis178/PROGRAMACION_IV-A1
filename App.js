fe4e73fc02bafaab1b3cc05e4613ea1f5ddd909f
Luis Quintanilla <luis99alexander@gmail.com>
2020-02-13 22:33:40 -0600

    ALUMNOS-OFFLINE

--- App.js
+++ App.js
@@ -1,43 +1,40 @@
-
-document.addEventListener("DOMContentLoaded", (event) =>{
+document.addEventListener("DOMContentLoaded",event =>{
     const formAlumnos = document.querySelector("#frmAlumno");
-    formAlumnos.addEventListener("submit", (e)=>{
-        e.preventDefault(); 
-        let codigo = document.querySelector("#txtCodigoAlumno").value,
-            nombre = document.querySelector("#txtNombreAlumno").value,
-            direccion = document.querySelector("#txtDireccionAlumno").value,
-            telefono = document.querySelector("#txtTelefonoAlumno").value;
+    formAlumnos.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        let Codigo = document.querySelector("#txtCodigoAlumno").value,
+        nombre = document.querySelector("#txtNombreAlumno").value,
+        direccion = document.querySelector("#txtDireccionAlumno").value,
+        telefono = document.querySelector("#txtTelefonoAlumno").value;
+
+        var keyCodigo = "codigo"+Codigo;
+        var keyNombre = "nombre"+Codigo;
+        var keyDireccion = "direccion"+Codigo;
+        var keyTelefono = "telefono"+Codigo;
+
         if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
-        } else {
-            alert("almacenamiento en local NO soportado!!! Actualizate!");
+            window.localStorage.setItem(keyCodigo,Codigo);
+            window.localStorage.setItem(keyNombre,nombre);
+            window.localStorage.setItem(keyDireccion,direccion);
+            window.localStorage.setItem(keyTelefono,telefono);
+        } else{
+            alert("El Almacenamiento en local no soporta, actualizate!");
         }
     });
     document.querySelector("#btnRecuperarAlumnos").addEventListener("click",(e)=>{
-        if( 'localStorage' in window ){
-            document.querySelector("#txtCodigoAlumno").value = 
-                window.localStorage.getItem("codigo");
-            document.querySelector("#txtNombreAlumno").value = 
-                window.localStorage.getItem("nombre");
-            document.querySelector("#txtDireccionAlumno").value = 
-                window.localStorage.getItem("direccion");
-            document.querySelector("#txtTelefonoAlumno").value = 
-                window.localStorage.getItem("telefono");
-        }  else {
-            alert("almacenamiento en local NO soportado!!! Actualizate!");
+        if('localStorage' in window){
+            let Codigo = document.querySelector("#txtCodigoAlumno").value;
+            if(Codigo != ""){
+            document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo"+Codigo);
+            document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre"+Codigo);
+            document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion"+Codigo);
+            document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono"+Codigo);
+            } else{
+                alert("PARA RECUPERAR LOS DATOS AGREGUE EL CODIGO");
+            }
+        }else{
+            alert("El Almacenamiento en local no soporta, actualizate!");
         }
     });
 });
-
-/*document.addEventListener("DOMContentLoaded",init);*/
-
-/*document.addEventListener("DOMContentLoaded",function(event){
-    alert("Pagina cargo forma 2");
-});*/
-
-/*function init(event){
-    alert("Hola la pagina a cargado");
-}*/
